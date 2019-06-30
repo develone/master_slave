0f688c1bb649cf818044ece58efd23af81441b5a
Edward Vidal Jr <develone@sbcglobal.net>
2019-06-30 04:57:07 -0600

    Now using separate files for main and thread.  master.c & mythread.c Now using master_slave header.

--- examples/build_svd.sh
+++ examples/build_svd.sh
@@ -5,7 +5,8 @@ gcc -g -O3 -c svd.c
 gcc -g -O3 -c disp_mat.c
 gcc -g -O3 -c trans_mat.c
 gcc -g -O3 -c mul_mat.c
+gcc -g -O3 -c mythread.c
 
 echo "Compiling master_svd"
-gcc -g -O3 master_svd.c svd.o disp_mat.o trans_mat.o mul_mat.o -lpthread -lm -o master_svd
+gcc -g -O3 master.c mythread.o svd.o disp_mat.o trans_mat.o mul_mat.o -lpthread -lm -o master
 
